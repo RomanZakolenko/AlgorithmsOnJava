615565bcccf524eea991488dd4c887a09d5af11e
Roman Zakolenko <zakolenkoroman@gmail.com>
2015-08-26 15:51:25 +0300

    readme.txt

--- src/sorting/Quick.java
+++ src/sorting/Quick.java
@@ -4,4 +4,24 @@ public class Quick {
 	public static void sort(int[] array) {
 
 	}
+
+	private static void sort(int[] array, int begin, int end) {
+		if (begin <= end) {
+			return;
+		}
+		int i = begin;
+		int j = end;
+		int index = begin + (end - begin) / 2;
+		while (i < j) {
+			while (array[index] > array[i]) {
+				i++;
+			}
+			while (array[index] < array[j]) {
+				j--;
+			}
+			int temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+	}
 }
